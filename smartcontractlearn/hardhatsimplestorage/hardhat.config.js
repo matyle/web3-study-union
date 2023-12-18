003b1882420a60cc5c3da5e03680babea108eb24
matytan <matytan@matytans-iMac.local>
2023-12-18 19:44:24 +0800

    add hardhat

--- smartcontractlearn/hardhatsimplestorage/hardhat.config.js
+++ smartcontractlearn/hardhatsimplestorage/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
   solidity: "0.8.19",
 };
